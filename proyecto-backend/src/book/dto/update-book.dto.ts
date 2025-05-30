56022f2bf32433630720a68a3fb0c17706d172a9
Daesfo27 <dsfontalvo-2032b@corhuila.edu.co>
2025-05-30 02:22:30 -0500

    carga data

--- proyecto-backend/src/book/dto/update-book.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookDto } from './create-book.dto';
-import { IsBoolean } from 'class-validator';
-
-export class UpdateBookDto extends PartialType(CreateBookDto) {
-    @IsBoolean()
-    active:Boolean
-}
--- proyecto-backend/src/message/dto/update-message.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMessageDto } from './create-message.dto';
-import { IsBoolean } from 'class-validator';
-
-export class UpdateMessageDto extends PartialType(CreateMessageDto) {
-    @IsBoolean()
-    active:Boolean
-}
